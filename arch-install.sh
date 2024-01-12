6b660ca6a7bd030e5e92fe323cbd97149a98a3ac
rainewallis <raine@rainew.dev>
2024-01-12 12:43:25 +0000

    prepend chroot command to each instruction

--- arch-install.sh
+++ arch-install.sh
@@ -3,6 +3,8 @@
 # BEFORE RUNNING THIS SCRIPT:
 # THE DISK MUST BE PARITIONED AND MOUNTED MANUALLY!!
 
+MOUNT_LOCATION="/mnt"
+
 # Get CPU Vendor
 CPU_VENDOR=$( lscpu | grep Vendor | awk '{print $NF}' )
 MICROCODE_PACKAGE=""
@@ -13,29 +15,30 @@ case "$CPU_VENDOR" in
 esac
 
 # Install base system elements
-pacstrap -K /mnt base base-devel linux linux-firmware sysfsutils usbutils e2fsprogs inetutils netctl vim less which man-db man-pages $MICROCODE_PACKAGE
+pacstrap -K $MOUNT_LOCATION base base-devel linux linux-firmware sysfsutils usbutils e2fsprogs inetutils netctl vim less which man-db man-pages $MICROCODE_PACKAGE
 
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U $MOUNT_LOCATION >> $MOUNT_LOCATION/etc/fstab
 
-arch-chroot /mnt
+# Change root command (must actually be prepended to each command, as is it will just drop you into bash)
+CHROOT="arch-chroot $MOUNT_LOCATION"
 
 # Install bootloader (source: https://arch.d3sox.me/installation/install-bootloader)
-pacman -S grub os-prober efibootmgr dosfstools mtools gptfdisk fatresize
-yes | grub-install --target=x86_64-efi --bootloader-id=grub-uefi --efi-directory=/boot --recheck
-grub-mkconfig -o /boot/grub/grub.cfg
+$CHROOT pacman -S grub os-prober efibootmgr dosfstools mtools gptfdisk fatresize
+$CHROOT grub-install --target=x86_64-efi --bootloader-id=grub-uefi --efi-directory=/boot --recheck
+$CHROOT grub-mkconfig -o /boot/grub/grub.cfg
 
 # Set to correct timezone
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock -systohc
+$CHROOT ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+$CHROOT hwclock -systohc
 
 # Set locale
-echo "en_GB.UTF-8 UTF-8" >> /etc/local.gen
-locale-gen
+$CHROOT echo "en_GB.UTF-8 UTF-8" >> /etc/local.gen
+$CHROOT locale-gen
 
-echo "LANG=en_GB.UTF-8" > /etc/locale.conf
-echo "KEYMAP=uk" > /etc/vconsole.conf
+$CHROOT echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+$CHROOT echo "KEYMAP=uk" > /etc/vconsole.conf
 
 # Save info for setup script
-echo "CPU_VENDOR=$CPU_VENDOR" > /root/.arch-install-vars.tmp
+$CHROOT echo "CPU_VENDOR=$CPU_VENDOR" > /root/.arch-install-vars.tmp
 
-passwd
+$CHROOT passwd

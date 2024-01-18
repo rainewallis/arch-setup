05acaa3a283d0acfc45fb8b304374bd0820ea2af
rainewallis <raine@rainew.dev>
2024-01-18 15:20:52 +0000

    Add new features

--- arch-setup.sh
+++ arch-setup.sh
@@ -4,7 +4,7 @@
 
 function yes_or_no {
     while true; do
-        read -p "$* [y/n]: " yn
+        read -rp "$* [y/n]: " yn
         case $yn in
             [Yy]*) return 0;;
             [Nn]*) echo "Aborted" ; return 1 ;;
@@ -47,12 +47,38 @@ then
     iwctl
 fi
 
-pacman -S bind lshw
-
+# Make sure the repos are up to date
+pacman -Syu
 
 #pacman -S networkmanager bind
 #systemctl enable NetworkManager
 #systemctl start NetworkManager
+pacman -S bind lshw plocate
+
+updatedb
+
+# Install yay to get access to AUR
+git clone https://aur.archlinux.org/yay.git
+
+WD=$( pwd )
+cd yay || return
+makepkg -si
+
+cd "$WD" || return
+
+# Setup user and sudo access
+useradd -m raine
+usermod -a -G wheel raine
+passwd raine
 
 # Get graphics vendor
 GRAPHICS_VENDOR=$( lshw -C display | grep vendor | awk -F: '{print $2}' )
+GRAPHICS_DRIVERS=""
+
+while IFS= read -r line; do
+    case $line in
+        *NVIDIA*) GRAPHICS_DRIVERS="nvidia" ;;
+    esac
+done <<< "$GRAPHICS_VENDOR"
+
+pacman -S hyprland hyprpaper waybar kitty pipewire btop "$GRAPHICS_DRIVERS"
